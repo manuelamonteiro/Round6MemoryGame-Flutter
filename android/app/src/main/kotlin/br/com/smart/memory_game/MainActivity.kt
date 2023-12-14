0d1485e4ff1565cfb95478c101724abd1b6772dc
Manuela Monteiro <manuella.m.p182@gmail.com>
2023-12-14 17:34:46 -0300

    initial project

---
+++ android/app/src/main/kotlin/br/com/smart/memory_game/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.smart.memory_game
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:memory_game/pages/home_page.dart';
+import 'package:memory_game/theme.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Round 6 Memory',
+      theme: Round6Theme.theme,
+      home: const HomePage(),
+    );
+  }
+}
